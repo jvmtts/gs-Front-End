85454811df8fa657ff586c0e83ea0fb54b9e7bfc
João <joaovictorrmattos@gmail.com>
2025-06-03 16:41:22 -0300

    menu humburguer

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', function(e) {
     const email = form.email.value.trim();
     const estado = form.estado.value;
 
-    if (email === '' || estado === '') {
+    if (!email || !estado) {
         message.textContent = 'Por favor, preencha todos os campos.';
         message.style.color = 'red';
     } else {
@@ -16,3 +16,25 @@ form.addEventListener('submit', function(e) {
         form.reset();
     }
 });
+    const navbar = document.querySelector('.nav-links');
+    const hamburger = document.createElement('div');
+    hamburger.classList.add('hamburger');
+    hamburger.innerHTML = '&#9776;';
+    document.querySelector('.navbar').appendChild(hamburger);
+
+    hamburger.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+});
+
+    function toggleMenuDisplay() {
+  if (window.innerWidth <= 768) {
+    hamburger.style.display = 'block';
+    navbar.style.display = navbar.classList.contains('active') ? 'block' : 'none';
+  } else {
+    hamburger.style.display = 'none';
+    navbar.style.display = 'flex';
+    navbar.classList.remove('active');
+  }
+}
+    toggleMenuDisplay();
+    window.addEventListener('resize', toggleMenuDisplay);
